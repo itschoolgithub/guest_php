1657cb7039154619d76131cea3e4c5d487c341cb
itschoolgithub <itschoolgithub@mail.ru>
2024-11-23 14:49:02 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container py-2">
+        <div class="row mb-2">
+            <div class="col">
+                <h1>Гостевая книга</h1>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col">
+
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">Дмитрий</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div class="text-secondary">12.03.2024 12:22</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <form action="" method="">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Имя</label>
+                        <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="comment" class="form-label">Комментарий</label>
+                        <textarea class="form-control" id="comment" name="comment"></textarea>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
